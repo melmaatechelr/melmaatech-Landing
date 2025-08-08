6f5e4df4808586fe6fc5107d07291439689f50aa
MelmaaTech_Eluru <128880871+melmaatechelr@users.noreply.github.com>
2025-08-08 09:10:21 +0530

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -65,7 +65,7 @@ const Footer = () => {
                   <img 
                     src="/assets/MelmaatechLogo.PNG" 
                     alt="Melmaa Tech" 
-                    className="h-10 w-auto"
+                    className="h-30 w-auto"
                     loading="lazy"
                   />
 
